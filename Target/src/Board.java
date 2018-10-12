e792935cd0d0c731fc7af7a9cf923013ade28b94
john.kearney <john.kearney@mu-c1-03.mullen.pvt.k12.co.us>
2018-10-12 15:03:51 -0600

    new

--- Target/src/Board.java
+++ Target/src/Board.java
@@ -1,4 +1,5 @@
 
+
 public class Board
 	{
 		
@@ -6,15 +7,21 @@ public class Board
 	
 	public static void display()
 		{
-			System.out.println(" __ __ __ __ __ __ __");
-			System.out.println("|  |  |  |  |  |  |  |");
-			System.out.println("----------------------");
-			System.out.println("|__|__|__|__|__|__|__|");
-			System.out.println("|__|__|__|__|__|__|__|");
-			System.out.println("|__|__|__|__|__|__|__|");
-			System.out.println("|__|__|__|__|__|__|__|");
-			System.out.println("|__|__|__|__|__|__|__|");
-			System.out.println("|__|__|__|__|__|__|__|");
+			System.out.println("-----------------------------");
+			System.out.println("| "  + board[0][0] +  " | " + board[0][1] + " | " + board[0][2] + " | " + board[0][3] + " | " + board[0][4] + " | " + board[0][5] + " | " + board[0][6] + " | ");
+			System.out.println("-----------------------------");
+			System.out.println("| "  + board[1][0] +  " | " + board[1][1] + " | " + board[1][2] + " | " + board[1][3] + " | " + board[1][4] + " | " + board[1][5] + " | " + board[1][6] + " | ");
+			System.out.println("-----------------------------");
+			System.out.println("| "  + board[2][0] +  " | " + board[2][1] + " | " + board[2][2] + " | " + board[2][3] + " | " + board[2][4] + " | " + board[2][5] + " | " + board[2][6] + " | ");
+			System.out.println("-----------------------------");
+			System.out.println("| "  + board[3][0] +  " | " + board[3][1] + " | " + board[3][2] + " | " + board[3][3] + " | " + board[3][4] + " | " + board[3][5] + " | " + board[3][6] + " | ");
+			System.out.println("-----------------------------");
+			System.out.println("| "  + board[4][0] +  " | " + board[4][1] + " | " + board[4][2] + " | " + board[4][3] + " | " + board[4][4] + " | " + board[4][5] + " | " + board[4][6] + " | ");
+			System.out.println("-----------------------------");
+			System.out.println("| "  + board[5][0] +  " | " + board[5][1] + " | " + board[5][2] + " | " + board[5][3] + " | " + board[5][4] + " | " + board[5][5] + " | " + board[5][6] + " | ");
+			System.out.println("-----------------------------");
+			System.out.println("| "  + board[6][0] +  " | " + board[6][1] + " | " + board[6][2] + " | " + board[6][3] + " | " + board[6][4] + " | " + board[6][5] + " | " + board[6][6] + " | ");
+			System.out.println("-----------------------------");
 							
 		}
 	}
--- Target/src/Game.java
+++ Target/src/Game.java
@@ -6,20 +6,30 @@ public class Game
 		{
 			    fillBoard();
 				Board.display();
-				
-
 			}
 		public static void fillBoard()
 			{
+				Scanner userInput = new Scanner(System.in);
+				boolean playing = true;
+				int randomrow = 0 ;
+                int randomcol = 0 ;
+                String roll1 = "";
+                String again = ""; 
+                /*System.out.println("Welcome, press enter to shoot.");
+                roll1 = userInput.nextLine();*/
 				for (int row = 0; row< Board.board.length; row++)
 					{
 						for(int col = 0; col< Board.board[0].length; col++)
 							{
-								Board.board [row] [col] = "o";
+								randomrow = (int) (Math.random() * 7) ;
+				                randomcol = (int) (Math.random() * 7) ;
+								Board.board [row] [col] = " ";
 								
 							}
 						System.out.println();
 					}
+				Board.board [randomrow] [randomcol] = "x";
 			}
+		
 
 	}
